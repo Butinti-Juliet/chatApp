062b123ee48676191e5ae3c74ba42508c5560f44
Butinti-Juliet <julietp238@gmail.com>
2019-09-20 16:04:32 -0700

    first commit

--- src/app/page/main/main.page.ts
+++ src/app/page/main/main.page.ts
@@ -6,6 +6,8 @@ import * as firebase from 'firebase';
 import { finalize } from 'rxjs/operators';
 import { AngularFireStorage } from '@angular/fire/storage';
 
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
 
 @Component({
   selector: 'app-main',
@@ -23,7 +25,7 @@ uid: string;
   downloadURL;
 
 
-  constructor(public af: AngularFireAuth, public fs: AngularFirestore,private storage: AngularFireStorage,private firebase: AngularFirestore) {
+  constructor(public af: AngularFireAuth, public fs: AngularFirestore,private storage: AngularFireStorage,private firebase: AngularFirestore,private camera: Camera) {
     // this.uid = localStorage.getItem('userid');
     this.uid=this.af.auth.currentUser.uid;
     this.chatRef = this.fs.collection('chat',ref=>ref.orderBy('Timestamp')).valueChanges();
@@ -46,30 +48,46 @@ if(this.text != ''){
 }
 }
 
-// upload(event) {
-//   const file= event.target.files[0];
-//  this.id = Math.random().toString(36).substring(2);
-// const filepath=this.id;
-// this.ref = this.storage.ref(filepath);
-// const task = this.storage.upload(filepath, file);
-// this.uploadState = task.percentageChanges();
-// task.snapshotChanges().pipe(
-//   finalize(() => {
-//     this.downloadURL = this.ref.getDownloadURL().subscribe(url=>{
-//        console.log(url);
-//       this.firebase.collection('chat').add({
-//         Name: this.af.auth.currentUser.displayName,
-//         photoURL:url,
-//         UserID: this.af.auth.currentUser.uid,
-//         // sendTo:this.sendto,
-//         Message: this.text,
-//         Timestamp:firebase.firestore.FieldValue.serverTimestamp(),    
-//         });
-//      })
-//    })
-//  ).subscribe();
+upload(event) {
+  const file= event.target.files[0];
+ this.id = Math.random().toString(36).substring(2);
+const filepath=this.id;
+this.ref = this.storage.ref(filepath);
+const task = this.storage.upload(filepath, file);
+this.uploadState = task.percentageChanges();
+task.snapshotChanges().pipe(
+  finalize(() => {
+    this.downloadURL = this.ref.getDownloadURL().subscribe(url=>{
+       console.log(url);
+      this.firebase.collection('chat').add({
+        Name: this.af.auth.currentUser.displayName,
+        photoURL:url,
+        UserID: this.af.auth.currentUser.uid,
+        // sendTo:this.sendto,
+        Message: this.text,
+        Timestamp:firebase.firestore.FieldValue.serverTimestamp(),    
+        });
+     })
+   })
+ ).subscribe();
 
-// }
+}
+
+takePhoto() {
+  const options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.FILE_URI,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+  this.camera.getPicture(options).then((imageData) => {
+   // imageData is either a base64 encoded string or a file URI
+   // If it's base64 (DATA_URL):
+   let base64Image = 'data:image/jpeg;base64,' + imageData;
+  }, (err) => {
+   // Handle error
+  });
+}
 
   ngOnInit() {
   }
